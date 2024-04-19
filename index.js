cbc239e5bc32c5e3db74394dd448edc1ff161a84
Dravez <167508277+Dravez@users.noreply.github.com>
2024-04-19 07:40:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let z = 5;
+z = 10;
+console.log(z)
+document.write(z)
+
+fname = 'onoriode';
+lname = 'uriri';
+console.log(fname + lname)
+document.write(fname + lname)
+window.alert(fname + lname)
